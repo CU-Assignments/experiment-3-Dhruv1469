7bb6f6e55288abcb95fe3b1d817e9286b5e115ab
Dhruv1469 <122140296+Dhruv1469@users.noreply.github.com>
2025-02-18 10:27:16 +0530

    Update exp33.java

--- exp33.java
+++ exp33.java
@@ -1,3 +1,19 @@
+/*
+Hard Level
+Problem Statement: Create a Java program for a university enrollment system with exception handling. The program should:
+
+Allow students to enroll in courses.
+Throw a CourseFullException if the maximum enrollment limit is reached.
+Throw a PrerequisiteNotMetException if the student hasn’t completed prerequisite courses.
+
+Input Example:
+Enroll in Course: Advanced Java
+Prerequisite: Core Java
+Status: Prerequisite not completed
+Output Example:
+Error: PrerequisiteNotMetException - Complete Core Java before enrolling in Advanced Java.
+
+*/
 import java.util.HashMap;
 import java.util.Scanner;
 
